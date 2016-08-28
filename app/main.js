1287841301c4fef493239194156800745501fb08
Victor Feijo <victor.feijoa@gmail.com>
2016-08-28 19:42:38 -0300

    created the board structure, added the simple canvas just for test

--- app/main.js
+++ app/main.js
@@ -1,18 +1,30 @@
 import { Enum } from './enum'
+import Board from './board'
 
 class Gomoku {
 
   constructor(playerName) {
     this.playerName = playerName
+    this.board = new Board()
     this.whoStart = Math.round(Math.random())
   }
 
   setup() {
     // create board, pieces, set initial state to game, etc
+    this.board.forEach((value, i, j, board) => {
+      board[i][j] = 'Blank Piece'
+    })
   }
 
   start() {
     // main loop game
+    this.board.forEach((value, i, j, board) => {
+      $('canvas').drawArc({
+        fillStyle: 'black',
+        x: i*80, y: j*100,
+        radius: 5
+      });
+    })
   }
 
   reset() {
@@ -20,7 +32,7 @@ class Gomoku {
   }
 }
 
-$('#alert').click(() => {
-  alert(Enum.BOARD_ROWS_NUMBER)
-})
+const gomoku = new Gomoku()
+gomoku.setup()
+gomoku.start()
 
--- public/bundle.js
+++ public/bundle.js
@@ -50,46 +50,56 @@
 	
 	var _enum = __webpack_require__(1);
 	
+	var _board = __webpack_require__(2);
+	
+	var _board2 = _interopRequireDefault(_board);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
-	var Foo = function () {
-	  function Foo(some) {
-	    _classCallCheck(this, Foo);
+	var Gomoku = function () {
+	  function Gomoku(playerName) {
+	    _classCallCheck(this, Gomoku);
 	
-	    this.some = some;
+	    this.playerName = playerName;
+	    this.board = new _board2.default();
+	    this.whoStart = Math.round(Math.random());
 	  }
 	
-	  _createClass(Foo, [{
-	    key: 'log',
-	    value: function log() {
-	      console.log(this.some);
+	  _createClass(Gomoku, [{
+	    key: 'setup',
+	    value: function setup() {
+	      // create board, pieces, set initial state to game, etc
+	      this.board.forEach(function (value, i, j, board) {
+	        board[i][j] = 'Blank Piece';
+	      });
 	    }
 	  }, {
-	    key: 'alert',
-	    value: function (_alert) {
-	      function alert() {
-	        return _alert.apply(this, arguments);
-	      }
-	
-	      alert.toString = function () {
-	        return _alert.toString();
-	      };
-	
-	      return alert;
-	    }(function () {
-	      alert(this.some);
-	    })
+	    key: 'start',
+	    value: function start() {
+	      // main loop game
+	      this.board.forEach(function (value, i, j, board) {
+	        $('canvas').drawArc({
+	          fillStyle: 'black',
+	          x: i * 80, y: j * 100,
+	          radius: 5
+	        });
+	      });
+	    }
+	  }, {
+	    key: 'reset',
+	    value: function reset() {
+	      // reset game
+	    }
 	  }]);
 	
-	  return Foo;
+	  return Gomoku;
 	}();
 	
-	var foo = new Foo('Foo');
-	document.getElementById('log').addEventListener('click', foo.log.bind(foo));
-	
-	$('#alert').click(function () {
-	  alert(_enum.Enum.BOARD_ROWS_NUMBER);
-	});
+	var gomoku = new Gomoku();
+	gomoku.setup();
+	gomoku.start();
 
 /***/ },
 /* 1 */
@@ -104,6 +114,63 @@
 	  BOARD_ROWS_NUMBER: 15,
 	  BOARD_COLUMNS_NUMBER: 15
 	};
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _enum = __webpack_require__(1);
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	var Board = function () {
+	  function Board() {
+	    _classCallCheck(this, Board);
+	
+	    this.board = new Array(_enum.Enum.BOARD_ROWS_NUMBER);
+	    this.cleanBoard();
+	  }
+	
+	  _createClass(Board, [{
+	    key: 'cleanBoard',
+	    value: function cleanBoard() {
+	      for (var i = 0; i < _enum.Enum.BOARD_ROWS_NUMBER; i++) {
+	        this.board[i] = new Array(_enum.Enum.BOARD_COLUMNS_NUMBER);
+	      }
+	    }
+	  }, {
+	    key: 'forEach',
+	    value: function forEach(func) {
+	      for (var i = 0; i < _enum.Enum.BOARD_ROWS_NUMBER; i++) {
+	        for (var j = 0; i < _enum.Enum.BOARD_ROWS_NUMBER; i++) {
+	          func(this.board[i][j], i, j, this.board);
+	        }
+	      }
+	    }
+	  }, {
+	    key: 'set',
+	    value: function set(func) {
+	      func(this.board);
+	    }
+	  }, {
+	    key: 'get',
+	    value: function get(row, column, func) {
+	      func(this.board, this.board[row][column]);
+	    }
+	  }]);
+	
+	  return Board;
+	}();
+	
+	exports.default = Board;
 
 /***/ }
 /******/ ]);
