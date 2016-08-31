a0b89d9c7ef83437081648b04ff1a21a3a09fc21
Matteus Legat <matteuslegat@gmail.com>
2016-08-31 00:09:03 -0300

    Immutable board class

--- app/enum.js
+++ app/enum.js
@@ -1,5 +1,19 @@
 export const Enum = {
   BOARD_ROWS_NUMBER: 15,
   BOARD_COLUMNS_NUMBER: 15,
-  ANIMATIONS_DURATION: 0
+  ANIMATIONS_DURATION: 0,
+  BOARD_WIDTH: 525,
+  BOARD_HEIGHT: 525,
+  KEY_LENGTH: 4,
+}
+
+export const Players = {
+  NONE: -1,
+  ONE: 0,
+  TWO: 1,
+}
+
+export const Type = {
+  HUMAN: 0,
+  AI: 1,
 }
--- app/type.js
+++
@@ -1,4 +0,0 @@
-export const Type = {
-  HUMAN: 0,
-  AI: 1,
-}
